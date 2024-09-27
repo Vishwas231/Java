040a7bae75cfe9984d1f7beabffcede50f65837c
Vishwas231 <161361911+Vishwas231@users.noreply.github.com>
2024-09-27 11:00:31 +0530

    variable and datatypes

---
+++ variables.java
@@ -0,0 +1,28 @@
+public class variables {
+    public static void main(String[] args) {
+        byte age = 25;                
+byte grade = 10;              
+byte temperature = -10;      
+byte hoursWorked = 40;        
+byte levels = 3;              
+byte speedLimit = 60;        
+byte daysInWeek = 7;          
+byte batteryPercentage = 85;
+
+
+short year = 2024;           
+short population = 15000;    
+short salary = 30000;        
+short distance = 1200;      
+short height = 170;         
+short booksRead = 45;        
+short transactionAmount = -500; 
+short daysInYear = 365;
+
+population = 16000;                
+ distance = 1900;          
+salary = 32000;                    
+daysInYear = 150;
+        System.out.println(population);
+    }
+}
