3a58eca8c7acc4b5e1973fb5ad186da5ae1d4244
Vishwas231 <161361911+Vishwas231@users.noreply.github.com>
2024-09-30 08:33:50 +0530

    Methods invoking

--- MethodSession.java
+++ MethodSession.java
@@ -24,5 +24,33 @@ public class MethodSession{
 		int numberTwo = 3;
 		int multiplicationOfNumbers = numberOne * numberTwo;
 		System.out.println(multiplicationOfNumbers);
+		display();
 	}
+	public static void display(){
+		System.out.println("This is a invoking a method");
+		division();
+	}
+	public static void division(){
+		int numberOne = 12;
+		int numberTwo = 3;
+		int divisionOfTwoNumbers = numberOne / numberTwo;
+		System.out.println(divisionOfTwoNumbers);
+		percentage();
+	}
+	public static void percentage(){
+		int value = 12;
+		int totalValue = 3;
+		int percentageOf = (value*100)/totalValue;
+		System.out.println(percentageOf);
+		orderSum();
+	}
+	public static void orderSum(){
+		int orderOne = 112;
+		int orderTwo = 11;
+		int orderThree = 123;
+		int orderFour = 115;
+		int totalOrder = orderOne - orderTwo + orderThree + orderFour;
+		System.out.println(totalOrder);
+	}
+
 }
